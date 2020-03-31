79105cbc08b7d6f504d15ef5a19fafbfaf784c06
wooody92 <pkower4@gmail.com>
2020-03-31 18:48:39 +0900

    [BE] refactor : 관측소 위치를 제공하는 메서드 이름변경
    - LocationController.java : getNearByStation()
     - 클라이언트로부터 위도, 경도 데이터를 받아 처리할 수 있도록 준비작업 진행했습니다.
    
    associate with #4

--- BE/dust12/src/main/java/dev/codesquad/java/dust12/LocationController.java
+++ BE/dust12/src/main/java/dev/codesquad/java/dust12/LocationController.java
@@ -3,6 +3,7 @@ package dev.codesquad.java.dust12;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -11,11 +12,15 @@ import java.io.IOException;
 public class LocationController {
 
     @GetMapping("/location")
-    public ResponseEntity<String> test() {
+    public ResponseEntity<String> getNearByStation() {
+        // @RequestBody String tmX, @RequestBody String tmY
+        final String dataKey = "stationName";
+        final String tempTmX = "244148.546388";
+        final String tempTmY = "412423.75772";
         try {
-            Location location = new Location("stationName");
-            StringBuilder sb = location.getLocationJsonData("244148.546388", "412423.75772");
-            String myData = (String) location.getParserData(sb);
+            Location location = new Location(dataKey);
+            StringBuilder openApiJsonData = location.getLocationJsonData(tempTmX, tempTmY);
+            String myData = (String) location.getParserData(openApiJsonData);
             return new ResponseEntity(location.ok(myData), HttpStatus.OK);
         } catch (IOException e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
