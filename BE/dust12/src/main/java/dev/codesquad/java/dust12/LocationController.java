b542817eaabac9494c9a5270db36905de4409ed8
wooody92 <pkower4@gmail.com>
2020-03-31 15:18:25 +0900

    [BE] feat : JSONObject 이용하여 특정 json 데이터 추출
    - Json 의존성 주입했습니다.
    - LocationController.java : getExtractData()
     - 공공 API 에서 받은 Json 데이터 중 원하는 데이터만 추출합니다.
    - LocationController.java : getLocationJsonData()
     - 공공 API 에서 전체 Json 데이터를 받아오는 메서드로 분리했습니다.
    
    associate with #4

--- BE/dust12/src/main/java/dev/codesquad/java/dust12/LocationController.java
+++ BE/dust12/src/main/java/dev/codesquad/java/dust12/LocationController.java
@@ -1,5 +1,7 @@
 package dev.codesquad.java.dust12;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,67 +9,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
 @RestController
 public class LocationController {
     private final String KEY = "57yLL991deZDKSPyN3LhskktRLww21pZhY84mpit%2FE3OkDPSKH5UJ5MOfnKDp24SOC3zCbISpcRTJnsneWmFYA%3D%3D";
-    private final String OPEN_API_URL = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList?tmX=244148.546388&tmY=412423.75772&_returnType=json&ServiceKey=";
-    private final String OPEN_API_URL2 = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList";
-    @GetMapping("/")
+    private final String OPEN_API_URL = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList";
+
+    @GetMapping("/test")
     public String test() {
-        StringBuilder result = new StringBuilder();
         try {
-            URL url = new URL(OPEN_API_URL + KEY);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-            urlConnection.setRequestMethod("GET");
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
-
-            String returnLine;
-            while ((returnLine = br.readLine()) != null) {
-                result.append(returnLine + "\n");
-            }
-            urlConnection.disconnect();
-            return result.toString();
-        } catch (Exception e) {
+            StringBuilder sb = getLocationJsonData("244148.546388", "412423.75772");
+            String jsonData = sb.toString();
+            String myData = (String) getExtractData(sb,"stationName");
+            return myData;
+        } catch (IOException e) {
             return e.getMessage();
         }
     }
 
-    @GetMapping("/test")
-    public String test2() {
-        try {
-            StringBuilder urlBuilder = new StringBuilder(OPEN_API_URL2); /*URL*/
-            urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + KEY); /*Service Key*/
-            urlBuilder.append("&" + URLEncoder.encode("tmX", "UTF-8") + "=" + URLEncoder.encode("244148.546388", "UTF-8")); /*TM측정방식 X좌표*/
-            urlBuilder.append("&" + URLEncoder.encode("tmY", "UTF-8") + "=" + URLEncoder.encode("412423.75772", "UTF-8")); /*TM측정방식 Y좌표*/
-            urlBuilder.append("&" + URLEncoder.encode("_returnType", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
+    private StringBuilder getLocationJsonData(String tmX, String tmY) throws IOException{
+        StringBuilder urlBuilder = new StringBuilder(OPEN_API_URL); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + KEY); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("tmX", "UTF-8") + "=" + URLEncoder.encode(tmX, "UTF-8")); /*TM측정방식 X좌표*/
+        urlBuilder.append("&" + URLEncoder.encode("tmY", "UTF-8") + "=" + URLEncoder.encode(tmY, "UTF-8")); /*TM측정방식 Y좌표*/
+        urlBuilder.append("&" + URLEncoder.encode("_returnType", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
 
-            URL url = new URL(urlBuilder.toString());
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Content-type", "application/json");
-            System.out.println("Response code: " + conn.getResponseCode());
-            BufferedReader rd;
-            if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            } else {
-                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-            }
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = rd.readLine()) != null) {
-                sb.append(line);
-            }
-            rd.close();
-            conn.disconnect();
-            System.out.println(sb.toString());
-            return sb.toString();
-        } catch (IOException e) {
-            return e.getMessage();
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+
+        return sb;
+    }
+
+    private Object getExtractData(StringBuilder sb, String data) {
+        JSONObject jsonObject = new JSONObject(sb.toString());
+        JSONArray jsonArray = (JSONArray) jsonObject.get("list");
+        return jsonArray.getJSONObject(0).get(data);
     }
 }
