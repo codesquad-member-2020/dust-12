6be622572c7835dae9484b961e74b06201a452a3
wooody92 <pkower4@naver.com>
2020-04-03 15:28:24 +0900

    [BE] feature : @CrossOrigin("*") 추가
    - ApiController.java : CORS error 없애기 위해 @CrossOrigin("*") 추가했습니다.
    
    associate with #17

--- BE/dust12/src/main/java/dev/codesquad/java/dust12/ApiController.java
+++ BE/dust12/src/main/java/dev/codesquad/java/dust12/ApiController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.List;
 import static dev.codesquad.java.dust12.ApiParam.NUMBER_OF_DATA;
 
 @RestController
+@CrossOrigin("*")
 public class ApiController {
     private final Logger logger = LoggerFactory.getLogger(ApiController.class);
     /*
